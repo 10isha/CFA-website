5a8323edd91fa7b5583f88a87e5d9334e7791613
Aviral-Sharma-2578 <aviralshar10@gmail.com>
2024-01-09 19:45:11 +0530

    landing page

---
+++ client/src/pages/LandingPage.js
@@ -0,0 +1,13 @@
+import React, {useState} from 'react'
+import {Nav} from '../components/Nav'
+import SideSection from '../components/SideSection'
+
+export default function LandingPage() {
+  return (
+    <div>
+      <Nav/>
+      <br/><br/>
+      <SideSection/>
+    </div>
+  )
+}
