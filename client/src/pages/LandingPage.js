ae2785584afe76658e50f9a974fca6d337600c4c
Aviral-Sharma-2578 <aviralshar10@gmail.com>
2024-01-09 23:13:18 +0530

    footer

--- client/src/pages/LandingPage.js
+++ client/src/pages/LandingPage.js
@@ -1,13 +1,24 @@
 import React, {useState} from 'react'
 import {Nav} from '../components/Nav'
 import SideSection from '../components/SideSection'
+import {Statistic} from '../components/Statistic'
+import Testimonial from '../components/Testimonial'
+import Footer from '../components/Footer'
 
 export default function LandingPage() {
   return (
     <div>
-      <Nav/>
+      <Nav />
       <br/><br/>
-      <SideSection/>
+      <SideSection />
+      <br/><br/>
+      <h1 className='sm:text-3xl text-2xl title-font font-medium text-gray-900 mt-4 px-5'>SA'23 Statistics</h1>
+      <Statistic />
+      <br/><br/>
+      <h1 className='sm:text-3xl text-2xl title-font font-medium text-gray-900 mt-4 px-5 -mb-8'>Testimonials</h1>
+      <Testimonial />
+      <br/><br/>
+      <Footer />
     </div>
   )
 }
